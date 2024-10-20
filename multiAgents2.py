f080032203b90498a845357a09055b32613dfa7b
diceCanHT <chtc429@gmail.com>
2024-10-19 21:16:47 -0700

    Update multiAgents2.py

--- multiAgents2.py
+++ multiAgents2.py
@@ -32,3 +32,22 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
             if beta <= alpha:
                 break  # Alpha-beta pruning
         return minEval, best_move
+        
+def negamax(game_status: GameStatus, depth: int, turn_multiplier: int, alpha=float('-inf'), beta=float('inf')):
+    terminal = game_status.is_terminal()
+    if (depth==0) or (terminal):
+        scores = game_status.get_negamax_scores(terminal)
+        return scores, None
+
+    negamaxEval = float('-inf')
+    best_move = None
+    for move in game_state.get_moves():
+        child_state = game_state.get_new_state(move)
+        eval_score, _ = -negamax(child_state, depth - 1, turn_multiplayer, alpha, beta)
+        if negamaxEval > eval_score:
+            negamaxEval = eval_score
+            best_move = move
+        alpha = max(alpha, negamxEval)
+        if beta <= alpha:
+            break # Alpha-beta pruning
+    return negamaxEval, best_move
